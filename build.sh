12a9d91a217b4fd46e4b0f53d9f7e935dc355883
xiaodong.yang <xiaodong.yang@szyh.com>
2024-11-13 18:52:38 +0800

    添加部署脚本

--- build.sh
+++ build.sh
@@ -2,13 +2,22 @@
 
 echo "pull go-admin"
 
-git clone git@github.com:yymagicer/go-admin.git
+if test -d "go-admin"; then
+  cd go-admin
+  git pull
+  cd ..
+else
+  git clone git@github.com:yymagicer/go-admin.git
+fi
 
 echo "pull go-admin-ui"
 
-git clone git@github.com:yymagicer/go-admin-ui.git
-
+if test -d "go-admin-ui"; then
+  cd go-admin-ui
+  git pull
+  cd ..
+else
+  git clone git@github.com:yymagicer/go-admin-ui.git
+fi
 
 sudo docker build -t registry.cn-hangzhou.aliyuncs.com/server-tool/go-admin-all:v1 .
-
-
